f58fdfc42feff52c7796bd800089627531e29911
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2025-02-27 10:36:02 +0100

    nerfed tile-based religions

--- data/BBM_Exploration.sql
+++ data/BBM_Exploration.sql
@@ -1,10 +1,35 @@
 --========================================================================================================================
 -- Religion beliefs
--- "Icons" belief only gives 1 relic instead of 2
 --========================================================================================================================
-	UPDATE ModifierArguments
-  SET Value = 1
-  WHERE ModifierId = "ATTACH_RELIC_CITY_STATE" and Name = "Amount";
+-- "Icons" reliquary belief only gives 1 relic instead of 2
+UPDATE ModifierArguments
+SET Value = 1
+WHERE ModifierId = "ATTACH_RELIC_CITY_STATE" and Name = "Amount";
+
+-- "Reincarnation" belief only gives 0.5 science per tropical instead of 2
+UPDATE ModifierArguments
+SET Value = 0.5
+WHERE ModifierId = "ATTACH_TROPICAL_CITIES_FOLLOWING_SCIENCE" and Name = "Amount";
+
+-- "Shamanism" belief only gives 0.5 culture per tundra instead of 2
+UPDATE ModifierArguments
+SET Value = 0.5
+WHERE ModifierId = "ATTACH_TUNDRA_CITIES_FOLLOWING_CULTURE" and Name = "Amount";
+
+-- "Desert Faith" belief only gives 1 gold per desert instead of 2
+UPDATE ModifierArguments
+SET Value = 1
+WHERE ModifierId = "ATTACH_DESERT_CITIES_FOLLOWING_GOLD" and Name = "Amount";
+
+-- "Sacred Herds" belief only gives 0.5 science per grassland instead of 1
+UPDATE ModifierArguments
+SET Value = 0.5
+WHERE ModifierId = "ATTACH_GRASSLAND_CITIES_FOLLOWING_SCIENCE" and Name = "Amount";
+
+-- "Sky Gods" belief only gives 0.5 culture per plains instead of 1
+UPDATE ModifierArguments
+SET Value = 0.5
+WHERE ModifierId = "ATTACH_PLAINS_CITIES_FOLLOWING_SCIENCE" and Name = "Amount";
 --========================================================================================================================
 --========================================================================================================================
 
--- localization/BBM_Exploration_LOC.sql
+++ localization/BBM_Exploration_LOC.sql
@@ -1,10 +1,35 @@
 --========================================================================================================================
 -- Religion beliefs
--- "Icons" belief only gives 1 relic instead of 2
 --========================================================================================================================
+-- "Icons" reliquary belief only gives 1 relic instead of 2
 UPDATE LocalizedText
 SET Text = "BBM | +1 Relic for first-time conversion of a City-State"
 WHERE Tag = "LOC_BELIEF_BONUS_9_DESCRIPTION"
+
+-- "Reincarnation" belief only gives 0.5 science per tropical instead of 2
+UPDATE LocalizedText
+SET Text = "BBM | +0.5[icon:YIELD_SCIENCE] Science for every Tropical tile in other Civilizations’ Settlements following your Religion."
+WHERE Tag = "LOC_BELIEF_BONUS_22_DESCRIPTION"
+
+-- "Shamanism" belief only gives 0.5 culture per tundra instead of 2
+UPDATE LocalizedText
+SET Text = "BBM | +0.5[icon:YIELD_CULTURE] Culture for every Tundra tile in other Civilizations’ Settlements following your Religion."
+WHERE Tag = "LOC_BELIEF_BONUS_23_DESCRIPTION"
+
+-- "Desert Faith" belief only gives 1 gold per desert instead of 2
+UPDATE LocalizedText
+SET Text = "BBM | +1[icon:YIELD_GOLD] Gold for every Desert tile in other Civilizations’ Settlements following your Religion."
+WHERE Tag = "LOC_BELIEF_BONUS_24_DESCRIPTION"
+
+-- "Sacred Herds" belief only gives 0.5 science per grassland instead of 1
+UPDATE LocalizedText
+SET Text = "BBM | +0.5[icon:YIELD_SCIENCE] Science for every Grassland tile in other Civilizations’ Settlements following your Religion."
+WHERE Tag = "LOC_BELIEF_BONUS_27_DESCRIPTION";
+
+-- "Sky Gods" belief only gives 0.5 culture per plains instead of 1
+UPDATE LocalizedText
+SET Text = "BBM | +0.5[icon:YIELD_CULTURE] Culture for every Plains tile in other Civilizations’ Settlements following your Religion."
+WHERE Tag = "LOC_BELIEF_BONUS_28_DESCRIPTION";
 --========================================================================================================================
 --========================================================================================================================
 
