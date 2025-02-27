ad4f350bbf5075ea48e2123860e57cf3461a3b17
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2025-02-27 10:49:20 +0100

    fixed quotes

--- localization/BBM_Exploration_LOC.sql
+++ localization/BBM_Exploration_LOC.sql
@@ -1,35 +1,35 @@
 --========================================================================================================================
 -- Religion beliefs
 --========================================================================================================================
--- "Icons" reliquary belief only gives 1 relic instead of 2
+-- 'Icons' reliquary belief only gives 1 relic instead of 2
 UPDATE LocalizedText
-SET Text = "BBM | +1 Relic for first-time conversion of a City-State"
-WHERE Tag = "LOC_BELIEF_BONUS_9_DESCRIPTION"
+SET Text = 'BBM | +1 Relic for first-time conversion of a City-State'
+WHERE Tag = 'LOC_BELIEF_BONUS_9_DESCRIPTION'
 
--- "Reincarnation" belief only gives 0.5 science per tropical instead of 2
+-- 'Reincarnation' belief only gives 0.5 science per tropical instead of 2
 UPDATE LocalizedText
-SET Text = "BBM | +0.5[icon:YIELD_SCIENCE] Science for every Tropical tile in other Civilizations’ Settlements following your Religion."
-WHERE Tag = "LOC_BELIEF_BONUS_22_DESCRIPTION"
+SET Text = 'BBM | +0.5[icon:YIELD_SCIENCE] Science for every Tropical tile in other Civilizations’ Settlements following your Religion.'
+WHERE Tag = 'LOC_BELIEF_BONUS_22_DESCRIPTION'
 
--- "Shamanism" belief only gives 0.5 culture per tundra instead of 2
+-- 'Shamanism' belief only gives 0.5 culture per tundra instead of 2
 UPDATE LocalizedText
-SET Text = "BBM | +0.5[icon:YIELD_CULTURE] Culture for every Tundra tile in other Civilizations’ Settlements following your Religion."
-WHERE Tag = "LOC_BELIEF_BONUS_23_DESCRIPTION"
+SET Text = 'BBM | +0.5[icon:YIELD_CULTURE] Culture for every Tundra tile in other Civilizations’ Settlements following your Religion.'
+WHERE Tag = 'LOC_BELIEF_BONUS_23_DESCRIPTION'
 
--- "Desert Faith" belief only gives 1 gold per desert instead of 2
+-- 'Desert Faith' belief only gives 1 gold per desert instead of 2
 UPDATE LocalizedText
-SET Text = "BBM | +1[icon:YIELD_GOLD] Gold for every Desert tile in other Civilizations’ Settlements following your Religion."
-WHERE Tag = "LOC_BELIEF_BONUS_24_DESCRIPTION"
+SET Text = 'BBM | +1[icon:YIELD_GOLD] Gold for every Desert tile in other Civilizations’ Settlements following your Religion.'
+WHERE Tag = 'LOC_BELIEF_BONUS_24_DESCRIPTION'
 
--- "Sacred Herds" belief only gives 0.5 science per grassland instead of 1
+-- 'Sacred Herds' belief only gives 0.5 science per grassland instead of 1
 UPDATE LocalizedText
-SET Text = "BBM | +0.5[icon:YIELD_SCIENCE] Science for every Grassland tile in other Civilizations’ Settlements following your Religion."
-WHERE Tag = "LOC_BELIEF_BONUS_27_DESCRIPTION";
+SET Text = 'BBM | +0.5[icon:YIELD_SCIENCE] Science for every Grassland tile in other Civilizations’ Settlements following your Religion.'
+WHERE Tag = 'LOC_BELIEF_BONUS_27_DESCRIPTION';
 
--- "Sky Gods" belief only gives 0.5 culture per plains instead of 1
+-- 'Sky Gods' belief only gives 0.5 culture per plains instead of 1
 UPDATE LocalizedText
-SET Text = "BBM | +0.5[icon:YIELD_CULTURE] Culture for every Plains tile in other Civilizations’ Settlements following your Religion."
-WHERE Tag = "LOC_BELIEF_BONUS_28_DESCRIPTION";
+SET Text = 'BBM | +0.5[icon:YIELD_CULTURE] Culture for every Plains tile in other Civilizations’ Settlements following your Religion.'
+WHERE Tag = 'LOC_BELIEF_BONUS_28_DESCRIPTION';
 --========================================================================================================================
 --========================================================================================================================
 
