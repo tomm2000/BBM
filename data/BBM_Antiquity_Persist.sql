0bbb098b6916b6b211d3709ffeefd50b45757034
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2025-03-03 23:38:36 +0100

    - fixed natural wonder yields - buffed fishing

--- data/BBM_Antiquity_Persist.sql
+++ data/BBM_Antiquity_Persist.sql
@@ -1,7 +1,6 @@
 --========================================================================================================================
 -- Natural wonders
 --========================================================================================================================
-
 -- Uluru: from +2 culture on desert tiles to +1 culture on desert tiles
 UPDATE Feature_CityYields
 SET YieldChange = 1
@@ -9,10 +8,11 @@ WHERE Feature_CityYieldType = 'ULURU_DESERT_CULTURE';
 
 -- ZHANGJIAJIE: from +2 culture on rough terrain tiles to +1 culture on rough terrain tiles
 UPDATE Feature_CityYields
-SET YieldType = 'YIELD_CULTURE'
+SET YieldChange = 1
 WHERE Feature_CityYieldType = 'ZHANGJIAJIE_ROUGH_CULTURE';
 --========================================================================================================================
 --========================================================================================================================
+
 
 --========================================================================================================================
 -- Spawn biases
@@ -35,20 +35,55 @@ WHERE CivilizationType = 'CIVILIZATION_MISSISSIPPIAN' AND TerrainType = 'TERRAIN
 
 
 --========================================================================================================================
--- Rural districts
+-- Farms & Granary
 --========================================================================================================================
 -- New: Farm 1 food adjacency for every adjacent farm
 INSERT INTO Constructible_Adjacencies (ConstructibleType, YieldChangeId, RequiresActivation)
 VALUES ('IMPROVEMENT_FARM', 'BBM_FarmChainFood', 0);
 
-INSERT INTO "Adjacency_YieldChanges" ("ID","AdjacentBiome","AdjacentConstructible","AdjacentConstructibleTag","AdjacentDistrict","AdjacentFeature","AdjacentFeatureClass","AdjacentLake","AdjacentNaturalWonder","AdjacentNavigableRiver","AdjacentQuarter","AdjacentResource","AdjacentResourceClass","AdjacentRiver","AdjacentSeaResource","AdjacentTerrain","AdjacentUniqueQuarter","AdjacentUniqueQuarterType","Age","ProjectMaxYield","Self","TilesRequired","YieldChange","YieldType")
+INSERT INTO 'Adjacency_YieldChanges' ('ID','AdjacentBiome','AdjacentConstructible','AdjacentConstructibleTag','AdjacentDistrict','AdjacentFeature','AdjacentFeatureClass','AdjacentLake','AdjacentNaturalWonder','AdjacentNavigableRiver','AdjacentQuarter','AdjacentResource','AdjacentResourceClass','AdjacentRiver','AdjacentSeaResource','AdjacentTerrain','AdjacentUniqueQuarter','AdjacentUniqueQuarterType','Age','ProjectMaxYield','Self','TilesRequired','YieldChange','YieldType')
 VALUES ('BBM_FarmChainFood',NULL,'IMPROVEMENT_FARM',NULL,NULL,NULL,NULL,0,0,0,0,0,'NO_RESOURCECLASS',0,0,NULL,0,NULL,NULL,1,0,1,1.0,'YIELD_GOLD');
 
 -- New: Farms get +1 production for adjacent granaries
 INSERT INTO Constructible_Adjacencies (ConstructibleType, YieldChangeId, RequiresActivation)
 VALUES ('IMPROVEMENT_FARM', 'BBM_FarmGranaryProduction', 0);
 
-INSERT INTO "Adjacency_YieldChanges" ("ID","AdjacentBiome","AdjacentConstructible","AdjacentConstructibleTag","AdjacentDistrict","AdjacentFeature","AdjacentFeatureClass","AdjacentLake","AdjacentNaturalWonder","AdjacentNavigableRiver","AdjacentQuarter","AdjacentResource","AdjacentResourceClass","AdjacentRiver","AdjacentSeaResource","AdjacentTerrain","AdjacentUniqueQuarter","AdjacentUniqueQuarterType","Age","ProjectMaxYield","Self","TilesRequired","YieldChange","YieldType")
+INSERT INTO 'Adjacency_YieldChanges' ('ID','AdjacentBiome','AdjacentConstructible','AdjacentConstructibleTag','AdjacentDistrict','AdjacentFeature','AdjacentFeatureClass','AdjacentLake','AdjacentNaturalWonder','AdjacentNavigableRiver','AdjacentQuarter','AdjacentResource','AdjacentResourceClass','AdjacentRiver','AdjacentSeaResource','AdjacentTerrain','AdjacentUniqueQuarter','AdjacentUniqueQuarterType','Age','ProjectMaxYield','Self','TilesRequired','YieldChange','YieldType')
 VALUES ('BBM_FarmGranaryProduction',NULL,'BUILDING_GRANARY',NULL,NULL,NULL,NULL,0,0,0,0,0,'NO_RESOURCECLASS',0,0,NULL,0,NULL,NULL,1,0,1,1.0,'YIELD_PRODUCTION');
+--========================================================================================================================
+--========================================================================================================================
+
+
+--========================================================================================================================
+-- Fishing Quay & Fishing boat
+--========================================================================================================================
+-- New: Fishing boat gets +2 gold for adjacent fishing quay
+-- INSERT INTO Constructible_Adjacencies (ConstructibleType, YieldChangeId, RequiresActivation)
+-- VALUES ('IMPROVEMENT_FISHING_BOAT', 'BBM_BoatQuayGold', 0);
+
+-- INSERT INTO Adjacency_YieldChanges ('ID','AdjacentBiome','AdjacentConstructible','AdjacentConstructibleTag','AdjacentDistrict','AdjacentFeature','AdjacentFeatureClass','AdjacentLake','AdjacentNaturalWonder','AdjacentNavigableRiver','AdjacentQuarter','AdjacentResource','AdjacentResourceClass','AdjacentRiver','AdjacentSeaResource','AdjacentTerrain','AdjacentUniqueQuarter','AdjacentUniqueQuarterType','Age','ProjectMaxYield','Self','TilesRequired','YieldChange','YieldType')
+-- VALUES ('BBM_BoatQuayGold',NULL,'BUILDING_FISHING_QUAY',NULL,NULL,NULL,NULL,0,0,0,0,0,'NO_RESOURCECLASS',0,0,NULL,0,NULL,NULL,1,0,1,2.0,'YIELD_GOLD');
+
+-- on resource boats too
+-- INSERT INTO Constructible_Adjacencies (ConstructibleType, YieldChangeId, RequiresActivation)
+-- VALUES ('IMPROVEMENT_FISHING_BOAT_RESOURCE', 'BBM_BoatResourceQuayGold', 0);
+
+-- INSERT INTO 'Adjacency_YieldChanges' ('ID','AdjacentBiome','AdjacentConstructible','AdjacentConstructibleTag','AdjacentDistrict','AdjacentFeature','AdjacentFeatureClass','AdjacentLake','AdjacentNaturalWonder','AdjacentNavigableRiver','AdjacentQuarter','AdjacentResource','AdjacentResourceClass','AdjacentRiver','AdjacentSeaResource','AdjacentTerrain','AdjacentUniqueQuarter','AdjacentUniqueQuarterType','Age','ProjectMaxYield','Self','TilesRequired','YieldChange','YieldType')
+-- VALUES ('BBM_BoatResourceQuayGold',NULL,'BUILDING_FISHING_QUAY',NULL,NULL,NULL,NULL,0,0,0,0,0,'RESOURCE_FISH',0,0,NULL,0,NULL,NULL,1,0,1,1.0,'YIELD_GOLD');
+
+-- New: Fishing quay +2 gold on fishing boats. warehouse bonus
+INSERT INTO Constructible_WarehouseYields (ConstructibleType, YieldChangeId, RequiresActivation)
+VALUES ('BUILDING_FISHING_QUAY', 'BBM_FishingQuayBoatGold', 0);
+
+INSERT INTO Warehouse_YieldChanges ('ID', 'Age', 'BiomeInCity', 'ConstructibleInCity', 'DistrictInCity', 'FeatureClassInCity', 'FeatureInCity', 'LakeInCity', 'MinorRiverInCity', 'NaturalWonderInCity', 'NavigableRiverInCity', 'Overbuilt', 'ResourceInCity', 'RouteInCity', 'TerrainInCity', 'TerrainTagInCity', 'YieldChange', 'YieldType')
+VALUES ('BBM_FishingQuayBoatGold', NULL, NULL, 'IMPROVEMENT_FISHING_BOAT', NULL, NULL, NULL, 0, 0, 0, 0, 0, 1, 0, NULL, NULL, 2, 'YIELD_GOLD')
+
+-- On resource boats too
+-- INSERT INTO Constructible_WarehouseYields (ConstructibleType, YieldChangeId, RequiresActivation)
+-- VALUES ('BUILDING_FISHING_QUAY', 'BBM_FishingQuayResourceBoatGold', 0);
+
+-- INSERT INTO Warehouse_YieldChanges ('ID','Age','BiomeInCity','ConstructibleInCity','DistrictInCity','FeatureClassInCity','FeatureInCity','LakeInCity','MinorRiverInCity','NaturalWonderInCity','NavigableRiverInCity','Overbuilt','ResourceInCity','RouteInCity','TerrainInCity','TerrainTagInCity','YieldChange','YieldType')
+-- VALUES ('BBM_FishingQuayResourceBoatGold',NULL,NULL,'IMPROVEMENT_FISHING_BOAT',NULL,NULL,NULL,0,0,0,0,0,1,0,NULL,NULL,2,'YIELD_GOLD')
+
 --========================================================================================================================
 --========================================================================================================================
--- localization/BBM_Antiquity_Persist_LOC.sql
+++ localization/BBM_Antiquity_Persist_LOC.sql
@@ -7,9 +7,17 @@ UPDATE LocalizedText
 SET Text = 'BBM : +6[icon:YIELD_HAPPINESS] Happiness per Age. +1[icon:YIELD_CULTURE] Culture on Desert Terrain in this Settlement.'
 WHERE Tag = 'LOC_FEATURE_ULURU_DESCRIPTION';
 
+UPDATE LocalizedText
+SET Text = 'BBM : +1[icon:YIELD_CULTURE] Culture on Desert Terrain in this Settlement.'
+WHERE Tag = 'LOC_FEATURE_ULURU_TOOLTIP';
+
 -- ZHANGJIAJIE: from +2 culture on rough terrain tiles to +1 culture on rough terrain tiles
 UPDATE LocalizedText
 SET Text = 'BBM : +2[icon:YIELD_HAPPINESS] Happiness and +4[icon:YIELD_PRODUCTION] Production per Age. +1[icon:YIELD_HAPPINESS] Culture on Rough Terrain in this Settlement.'
-WHERE Tag = 'LOC_FEATURE_ZHANGJIAJIE_DESCRIPTION'
+WHERE Tag = 'LOC_FEATURE_ZHANGJIAJIE_DESCRIPTION';
+
+UPDATE LocalizedText
+SET Text = 'BBM : +1[icon:YIELD_CULTURE] Culture on Rough Terrain in this Settlement.'
+WHERE Tag = 'LOC_FEATURE_ZHANGJIAJIE_TOOLTIP'
 --========================================================================================================================
 --========================================================================================================================
