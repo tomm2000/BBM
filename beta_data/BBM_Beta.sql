75ed25fc3218413583b6bb1f215c90e9a00767a2
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2025-03-05 11:24:56 +0100

    - completed bath/garden buff - added grassland buff to standard rules

--- beta_data/BBM_Beta.sql
+++ beta_data/BBM_Beta.sql
@@ -1,50 +1,38 @@
-UPDATE TerrainBiomeFeature_YieldChanges
-SET YieldChange = 2
-WHERE YieldChange = 1
-  AND NOT TerrainType = 'TERRAIN_NAVIGABLE_RIVER'
-  AND BiomeType = 'BIOME_GRASSLAND'
-  AND YieldType = 'YIELD_FOOD'
-  AND ScaleByGameAge = 0;
-
-
--- INSERT INTO "Constructible_Adjacencies" ("ConstructibleType", "YieldChangeId")
--- SELECT "ConstructibleType", 'BBM_RuralBathAdjacency'
--- FROM "Constructible_ValidDistricts"
--- WHERE "DistrictType" = 'DISTRICT_RURAL';
-
--- DELETE if exists
-DELETE FROM Adjacency_YieldChanges
-WHERE ID = 'BBM_RuralBathAdjacency';
-
+--========================================================================================================================
+-- Bath and garden buff
+--========================================================================================================================
+-- Rural districts get +1 culture from adjacent baths
 INSERT INTO 'Adjacency_YieldChanges' ('ID','AdjacentBiome','AdjacentConstructible','AdjacentConstructibleTag','AdjacentDistrict','AdjacentFeature','AdjacentFeatureClass','AdjacentLake','AdjacentNaturalWonder','AdjacentNavigableRiver','AdjacentQuarter','AdjacentResource','AdjacentResourceClass','AdjacentRiver','AdjacentSeaResource','AdjacentTerrain','AdjacentUniqueQuarter','AdjacentUniqueQuarterType','Age','ProjectMaxYield','Self','TilesRequired','YieldChange','YieldType')
 VALUES ('BBM_RuralBathAdjacency',NULL,'BUILDING_BATH',NULL,NULL,NULL,NULL,0,0,0,0,0,'NO_RESOURCECLASS',0,0,NULL,0,NULL,NULL,1,0,1,1.0,'YIELD_CULTURE');
 
 INSERT INTO "Constructible_Adjacencies" ("ConstructibleType", "YieldChangeId")
 VALUES
-('IMPROVEMENT_FARM', 'BBM_RuralBathAdjacency');
+('IMPROVEMENT_FARM', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_PASTURE', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_PLANTATION', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_MINE', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_QUARRY', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_CLAY_PIT', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_WOODCUTTER', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_CAMP', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_FISHING_BOAT', 'BBM_RuralBathAdjacency')
+('IMPROVEMENT_OIL_RIG', 'BBM_RuralBathAdjacency');
 
-INSERT INTO "Constructible_Adjacencies" ("ConstructibleType", "YieldChangeId")
-VALUES
-('IMPROVEMENT_WOODCUTTER', 'BBM_RuralBathAdjacency');
-
-
--- INSERT INTO "Constructible_Adjacencies" ("ConstructibleType", "YieldChangeId")
--- SELECT "ConstructibleType", 'BBM_RuralGardenAdjacency'
--- FROM "Constructible_ValidDistricts"
--- WHERE "DistrictType" = 'DISTRICT_RURAL';
-
--- DELETE if exists
-DELETE FROM Adjacency_YieldChanges
-WHERE ID = 'BBM_RuralGardenAdjacency';
-
+-- Rural districts get +2 food from adjacent gardens
 INSERT INTO 'Adjacency_YieldChanges' ('ID','AdjacentBiome','AdjacentConstructible','AdjacentConstructibleTag','AdjacentDistrict','AdjacentFeature','AdjacentFeatureClass','AdjacentLake','AdjacentNaturalWonder','AdjacentNavigableRiver','AdjacentQuarter','AdjacentResource','AdjacentResourceClass','AdjacentRiver','AdjacentSeaResource','AdjacentTerrain','AdjacentUniqueQuarter','AdjacentUniqueQuarterType','Age','ProjectMaxYield','Self','TilesRequired','YieldChange','YieldType')
 VALUES ('BBM_RuralGardenAdjacency',NULL,'BUILDING_GARDEN',NULL,NULL,NULL,NULL,0,0,0,0,0,'NO_RESOURCECLASS',0,0,NULL,0,NULL,NULL,1,0,1,2.0,'YIELD_FOOD');
 
 INSERT INTO "Constructible_Adjacencies" ("ConstructibleType", "YieldChangeId")
 VALUES
-('IMPROVEMENT_FARM', 'BBM_RuralGardenAdjacency');
-
-INSERT INTO "Constructible_Adjacencies" ("ConstructibleType", "YieldChangeId")
-VALUES
-('IMPROVEMENT_WOODCUTTER', 'BBM_RuralGardenAdjacency');
-
+('IMPROVEMENT_FARM', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_PASTURE', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_PLANTATION', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_MINE', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_QUARRY', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_CLAY_PIT', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_WOODCUTTER', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_CAMP', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_FISHING_BOAT', 'BBM_RuralGardenAdjacency')
+('IMPROVEMENT_OIL_RIG', 'BBM_RuralGardenAdjacency');
+--========================================================================================================================
+--========================================================================================================================
